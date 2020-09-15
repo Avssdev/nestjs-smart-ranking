aa74837e9e9cca901e825d3b2f10876aa5c73397
Alvaro Silva <avssdev.development@gmail.com>
2020-09-15 20:26:02 -0300

    Criação da estrutura desafio

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,14 @@ import { MongooseModule } from '@nestjs/mongoose';
 
 import { JogadoresModule } from './jogadores/jogadores.module';
 import { CategoriasModule } from './categorias/categorias.module';
+import { DesafiosModule } from './desafios/desafios.module';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://localhost:27017/smartranking', { useNewUrlParser: true, useCreateIndex: true, useUnifiedTopology: true, useFindAndModify: false }),
     JogadoresModule,
-    CategoriasModule
+    CategoriasModule,
+    DesafiosModule
   ],
   controllers: [],
   providers: [],
---
+++ src/desafios/desafios.module.ts
@@ -0,0 +1,13 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import { DesafiosService } from './desafios.service';
+import { DesafiosController } from './desafios.controller';
+import { DesafioSchema } from './interfaces/desafio.schema';
+
+@Module({
+  imports: [MongooseModule.forFeature([{ name: 'Desafio', schema: DesafioSchema  }])],
+  providers: [DesafiosService],
+  controllers: [DesafiosController]
+})
+export class DesafiosModule {}
